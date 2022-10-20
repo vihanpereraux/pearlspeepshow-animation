bc5d09e15b4936e782bbd999d4f74f95d29df84e
= <=>
2022-10-20 16:55:55 +0530

    Add  LiquidDistortMaterial

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ document.fonts.ready.then(() => {
 
     // getting screen widths in media query pov
     const mediaQueryOne = window.matchMedia('(min-width: 1024px)');
-    const mediaQueryTwo = window.matchMedia('(max-width: 768px)');
-    const mediaQueryThree = window.matchMedia('(max-width: 425px)');
 
     // laptop screens
     if(mediaQueryOne.matches){
@@ -20,10 +18,14 @@ document.fonts.ready.then(() => {
             family: "roc grotesk wide",
             size: 60,
             fill: "white",
+            paddingLeft: 100,
+            paddingRight: 100
         });
     }
 
     let material = new Blotter.RollingDistortMaterial();
+    let material2 = new Blotter.LiquidDistortMaterial();
+
     material.uniforms.uSineDistortSpread.value = 0.000;
     material.uniforms.uSineDistortCycleCount.value = 5;
     material.uniforms.uSineDistortAmplitude.value = 0.03;
@@ -31,8 +33,12 @@ document.fonts.ready.then(() => {
     material.uniforms.uNoiseDistortAmplitude.value = 0.004;
     material.uniforms.uRotation.value = 200;
     material.uniforms.uSpeed.value = 0.18;
+    
+    material2.uniforms.uSpeed.value = 0.5;
+    material2.uniforms.uVolatility.value = 0.02;
+    material2.uniforms.uSeed.value = 0.1;
 
-    let blotter = new Blotter(material, {
+    let blotter = new Blotter(material2, {
         texts: text
     });
 
